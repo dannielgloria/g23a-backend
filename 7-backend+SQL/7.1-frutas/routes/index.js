3b2b0fe838dc05f87c7d10ec230ef724032e335c
dannielgloria <danniel.gloria@gmail.com>
2023-08-23 20:04:10 -0600

    feat: se añade controladores a los endpoints

--- 7-backend+SQL/7.1-frutas/routes/index.js
+++ 7-backend+SQL/7.1-frutas/routes/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import frutaController from "../controller/fruta.controller";
 
 const router = express.Router();
+// Routes = EndPoins
 
-router.get() //express.Router().get() = app.get();
+router.get('/frutas/:id', frutaController.getFrutasById); //express.Router().get() = app.get();
+router.post('/frutas', frutaController.createFrutas);
+router.put('/frutas/:id', )
+router.delete('/frutas/:id', )
 
 export default {
     router
