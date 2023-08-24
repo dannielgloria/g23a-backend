d26ebcef6b0b4170cad7cb60dd99707602633a6d
dannielgloria <danniel.gloria@gmail.com>
2023-08-23 20:32:21 -0600

    feat: se crean los controladores faltantes

--- 7-backend+SQL/7.1-frutas/controller/fruta.controller.js
+++ 7-backend+SQL/7.1-frutas/controller/fruta.controller.js
@@ -22,10 +22,40 @@ class FrutaController{
             return response.status(201).json(fruta)
         } catch (error) {
             console.error(error);
-            response.status(500).send('Internal Server Error');
+            return response.status(500).send('Internal Server Error');
         }
     }
 
+    async updateFruta(request, response){
+        try {
+            const id = request.params.id;
+            const update = request.body;
+            const success = await SERVICE
+
+            if (!success) {
+                return response.status(404).send('Fruta not found');
+            }
+            request.status(201).send('Fruta actualizada');
+        } catch (error) {
+            console.error(error);
+            return response.status(500).send('Internal Server Error');
+        }
+    }
+
+    async deleteFruta(request, response){
+        try {
+            const id = request.params.id;
+            const success = await SERVICE
+
+            if (!success) {
+                return response.status(404).send('Fruta not found');
+            }
+            return request.status(201).send('Fruta eliminada');
+        } catch (error) {
+            console.error(error);
+            return response.status(500).send('Internal Server Error');
+        }
+    }
 }
 
 export default new FrutaController();
