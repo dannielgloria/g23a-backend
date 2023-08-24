4fc20b5ce765784837db5c48abea43999289e5b1
dannielgloria <danniel.gloria@gmail.com>
2023-08-23 19:19:33 -0600

    feat: se crea la estructura basica para las rutas
    --Las rutas son nuestros endpoints

---
+++ 7-backend+SQL/7.1-frutas/index.js
@@ -0,0 +1,9 @@
+import express from "express";// const express = require('express')
+import router from "./routes";
+
+const app = express();
+app.use(express.json());
+
+app.use(router)// se conectan las rutas que estan en la carpeta routes
+
+app.listen(8080, ()=> console.log('Server listening on port 8080'))
