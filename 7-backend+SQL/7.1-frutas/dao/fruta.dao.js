2c8debaa7527523cb9e1fe1e02aaed82452491cc
dannielgloria <danniel.gloria@gmail.com>
2023-08-23 21:07:38 -0600

    feat: se crean los DAO para fruta

---
+++ 7-backend+SQL/7.1-frutas/dao/fruta.dao.js
@@ -0,0 +1,45 @@
+import db from "../database/db";
+
+class FrutaDAO{
+
+    async createFruta(nombre,descripcion,cantidad,precio){
+        const [id] = await db('frutas')
+            .insert({
+                nombre,
+                descripcion,
+                cantidad,
+                precio
+            })
+            .returning('id');
+        
+        return id
+    }
+
+    async getFrutaById(id){
+        const fruta = await db('frutas')
+            .select('*')
+            .where({id})
+            .first();
+
+        return fruta
+    }
+
+    async updateFruta(id, updates){
+        await db('frutas')
+            .where({id})
+            .update(updates);
+            
+        return true
+    }
+
+    async deleteFruta(id){
+        await db('frutas')
+            .where({id})
+            .del();
+        
+        return true
+    }
+
+}
+
+export default new FrutaDAO();
