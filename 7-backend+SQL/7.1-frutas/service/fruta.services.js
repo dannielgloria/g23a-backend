df2eff957f0267979ac31f9cf1b138e4629bad2a
dannielgloria <danniel.gloria@gmail.com>
2023-08-23 21:09:17 -0600

    feat: se asignan los dao a nuestros services

--- 7-backend+SQL/7.1-frutas/service/fruta.services.js
+++ 7-backend+SQL/7.1-frutas/service/fruta.services.js
@@ -1,21 +1,22 @@
+import frutaDao from "../dao/fruta.dao"
 
 class FrutaService{
     async createFruta(frutaDto){
         const {nombre,descripcion,cantidad,precio} = frutaDto
-        return FRUTADAO.createFruta(nombre,descripcion,cantidad,precio)
+        return frutaDao.createFruta(nombre,descripcion,cantidad,precio)
     }
 
     async getFrutaById(id){
-        return FRUTADAO.getFrutaById(id)
+        return frutaDao.getFrutaById(id)
     }
 
     async updateFruta(id, updates){
     
-        return FRUTADAO.updateFruta(id, updates)
+        return frutaDao.updateFruta(id, updates)
     }
 
     async deleteFruta(id){
-        return FRUTADAO.deleteFruta(id)
+        return frutaDao.deleteFruta(id)
     }
 }
 
